76c7abcbfb2ac70ffa18d5de6164acc1fced7bb8
aventador <303639161@qq.com>
2017-12-29 15:58:40 -0500

    The header is not working properly

--- PROJECT/src/app/shopping-list/shopping-list.component.ts
+++ PROJECT/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
   ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10)
@@ -16,5 +17,10 @@ export class ShoppingListComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
+
 
 }

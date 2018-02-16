37e4147068e3e2d4577bf79092ec368d48d4c357
aventador <303639161@qq.com>
2018-02-15 23:36:25 -0500

    clean-up

--- PROJECT/src/app/recipes/recipe.service.ts
+++ PROJECT/src/app/recipes/recipe.service.ts
@@ -1,9 +1,9 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
+@Injectable()
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
         new Recipe('Tasty Schnitzel', 
